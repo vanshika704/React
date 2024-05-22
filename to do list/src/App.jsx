c7843a70d97c3f9db5b42f5706a98e487c6d3dfc
vanshika704 <sharmavanshi704@gmail.com>
2024-05-22 19:02:27 +0530

    todo

--- to do list/src/App.jsx
+++ to do list/src/App.jsx
@@ -1,12 +1,19 @@
-function App() {
- 
+import './App.css';
+import './index.css';
 
+function App() {
   return (
-    <><h1>
-      Welcome !!!!
-    </h1>
-      </>
-  )
+    <>
+      <h1 className="top-center">TODO APP</h1>
+      <EnterTodo />
+    </>
+  );
 }
 
-export default App
+export default App;
+
+function EnterTodo() {
+  return (
+    <input className="input-button" type="text" placeholder="Enter To Do Here" />
+  );
+}
