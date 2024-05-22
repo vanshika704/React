cab91d2525967d5c514d0c81e54ba3b9570c58bc
vanshika704 <sharmavanshi704@gmail.com>
2024-05-22 22:19:56 +0530

    changes

--- to do list/src/App.jsx
+++ to do list/src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
@@ -10,10 +13,36 @@ function App() {
   );
 }
 
-export default App;
-
 function EnterTodo() {
+  const handleAddTodo = () => {
+    const inputField = document.querySelector('.input-button');
+    const todoText = inputField.value;
+    console.log("Todo added:", todoText);
+
+   
+    inputField.value = "";
+
+    
+    const todoList = document.querySelector('.todo-list');
+    const todoItem = document.createElement('div');
+    todoItem.textContent = todoText;
+    todoList.appendChild(todoItem);
+  };
+
   return (
-    <input className="input-button" type="text" placeholder="Enter To Do Here" />
+    <div className="input-container">
+      <input 
+        className="input-button" 
+        type="text" 
+        placeholder="Enter To Do Here" 
+      />
+      <button type="button" className="btn btn-dark" onClick={handleAddTodo}>
+        <FontAwesomeIcon icon={faCheck} />
+      </button>
+      <br></br>
+      <div className="todo-list"></div>
+    </div>
   );
 }
+
+export default App;
