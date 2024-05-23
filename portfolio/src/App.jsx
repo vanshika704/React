c3491fe3190bd5ee16883095b5ffa728528b552c
vanshika704 <sharmavanshi704@gmail.com>
2024-05-24 01:17:44 +0530

    social

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,25 +1,34 @@
-
-import './index.css'; 
+import "./index.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'; 
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
-  );
-}
-
-function NavBar() {
-  return (
-    <nav className="navbar">
-      <ul className="elements">
-        <li className="home">Home</li>
-        <li className="about">About</li>
-        <li className="blogs">Blogs</li>
-        <li className="skills">Skills</li>
-      </ul>
-    </nav>
+    <>
+      <Card1 />
+    </>
   );
 }
 
 export default App;
+
+function Card1() {
+  return (
+    <div className="centered-card">
+      <div className="card-content">
+        <div>
+          <ul>
+            <li>
+              <div>Vanshika Sharma</div><hr></hr>
+            </li>
+            <li className="social">
+              <FontAwesomeIcon icon={faInstagram} /> 
+              <FontAwesomeIcon icon={faLinkedin} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
