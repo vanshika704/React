33d3da3b39efcd4775a6a036646214020f819eb7
vanshika704 <sharmavanshi704@gmail.com>
2024-05-23 17:09:04 +0530

    todo ui

--- to do list/src/App.jsx
+++ to do list/src/App.jsx
@@ -1,48 +1,15 @@
-import './App.css';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-
-function App() {
-  return (
-    <>
-      <h1 className="top-center">TODO APP</h1>
-      <EnterTodo />
-    </>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App(){
+  return(
+    <><center className="todo-container"><h1>Todo App</h1>
+    <div className="container text-center">
+  
+  <div className="row">
+    <div className="col-6"><input type="text" placeholder='Enter todo' /></div>
+    <div className="col-4"><input type="date" /></div>
+    <div className="col-2"><button type="button" className="btn btn-success">Add</button></div>
+  </div>
+</div></center></>
+  )
 }
-
-function EnterTodo() {
-  const handleAddTodo = () => {
-    const inputField = document.querySelector('.input-button');
-    const todoText = inputField.value;
-    console.log("Todo added:", todoText);
-
-   
-    inputField.value = "";
-
-    
-    const todoList = document.querySelector('.todo-list');
-    const todoItem = document.createElement('div');
-    todoItem.textContent = todoText;
-    todoList.appendChild(todoItem);
-  };
-
-  return (
-    <div className="input-container">
-      <input 
-        className="input-button" 
-        type="text" 
-        placeholder="Enter To Do Here" 
-      />
-      <button type="button" className="btn btn-dark" onClick={handleAddTodo}>
-        <FontAwesomeIcon icon={faCheck} />
-      </button>
-      <br></br>
-      <div className="todo-list"></div>
-    </div>
-  );
-}
-
 export default App;
