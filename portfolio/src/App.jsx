4e6cf5607fcd279191ea378004bb32ab1ac700f0
vanshika704 <sharmavanshi704@gmail.com>
2024-05-24 01:47:42 +0530

    commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import "./index.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'; 
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'; 
+//import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -20,15 +20,26 @@ function Card1() {
         <div>
           <ul>
             <li>
-              <div>Vanshika Sharma</div><hr></hr>
+              <div>Vanshika Sharma</div>
+              <hr />
             </li>
             <li className="social">
               <FontAwesomeIcon icon={faInstagram} /> 
               <FontAwesomeIcon icon={faLinkedin} />
+              <FontAwesomeIcon icon={faTwitter} />
+              <div className="card" style={{ width: "18rem" }}>
+              <img src="assets/photo.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Profile</h5>
+              <p className="card-text">Explore Profile</p>
+              <button className="Button">Lets Go!!</button>
+            </div></div>
             </li>
           </ul>
+         
+          </div>
         </div>
       </div>
-    </div>
+    
   );
 }
