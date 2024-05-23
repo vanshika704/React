e083d553b46d0a44394e929aed8fb598f5ccac80
vanshika704 <sharmavanshi704@gmail.com>
2024-05-23 17:14:18 +0530

    .

--- to do list/src/App.jsx
+++ to do list/src/App.jsx
@@ -9,6 +9,16 @@ function App(){
     <div className="col-4"><input type="date" /></div>
     <div className="col-2"><button type="button" className="btn btn-success">Add</button></div>
   </div>
+  <div className="row">
+    <div className="col-6">Buy Milk</div>
+    <div className="col-4">4/10/2023</div>
+    <div className="col-2"><button type="button" className="btn btn-danger">Remove</button></div>
+  </div>
+  <div className="row">
+    <div className="col-6">Go to college</div>
+    <div className="col-4">4/10/2023</div>
+    <div className="col-2"><button type="button" className="btn btn-success">Add</button></div>
+  </div>
 </div></center></>
   )
 }
