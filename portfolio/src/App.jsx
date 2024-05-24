1893e09aad692ce1d4dfccd20c9535ca9f71c217
vanshika704 <sharmavanshi704@gmail.com>
2024-05-24 13:47:48 +0530

    newwww

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,42 +1,83 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
     <>
-      <Card1 />
+      <Navbar />
     </>
   );
 }
 
 export default App;
 
-function Card1() {
+function Navbar() {
   return (
-    <div className="centered-card">
-      <div className="card-content">
-        <div>
-          <ul>
-            <li>
-              <div>Vanshika Sharma</div>
-              <hr />
-            </li>
-            <li className="social">
-              <FontAwesomeIcon icon={faInstagram} /> 
-              <FontAwesomeIcon icon={faLinkedin} />
-              <FontAwesomeIcon icon={faTwitter} />
-              <Image/>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+         Vanshika Sharma
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
             
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Skills 
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown link
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
-         
-          </div>
         </div>
       </div>
-  
+    </nav>
   );
-}
-function Image(){
-  return(<><div className="imagephotu"></div></>)
 }
