23b11bfe99493dc87d4e4c3ded63dfea77f31caa
vanshika704 <sharmavanshi704@gmail.com>
2024-05-24 22:52:58 +0530

    new

--- to do list/src/App.jsx
+++ to do list/src/App.jsx
@@ -15,7 +15,7 @@ export default App;
 
 function Enterycard() {
   return (
-    <div className="card" style={{ width: '30rem', margin: '10px' }}>
+    <div className="card" style={{ width: '30rem', margin: '10px' ,height:'40rem'}}>
       <img src={todoImage} className="card-img-top" alt="Todo" />
       <div className="card-body">
         <h5 className="card-title">TO-DO Items</h5>
@@ -29,29 +29,26 @@ function Enterycard() {
         <li className="list-group-item"><input type='time' /></li>
         <li className="list-group-item"><input type="text" placeholder='enter specific details' /></li>
       </ul>
-      <div className="card-body">
-        <a href="#" className="card-link">Card link</a>
-        <a href="#" className="card-link">Another link</a>
-      </div>
+    
     </div>
   );
 }
 
 function Card2() {
   return (
-    <div className="card" style={{ width: '30rem', margin: '10px' }}>
+    <div className="card" style={{ width: '30rem', margin: '10px',height:'40rem' }}>
       <img src={download} className="card-img-top" alt="Todo" />
       <div className="card-body">
         <h5 className="card-title">TO-DO Items</h5>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <p className="card-text" style={{ marginRight: '10px' }}>Enter Here</p>
+          <p className="card-text" style={{ marginRight: '10px' }}>Your chores..</p>
           
         </div>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item"></li>
-        <li className="list-group-item"></li>
-        <li className="list-group-item"></li>
+        <li className="list-group-item">Clean the dishes</li>
+        <li className="list-group-item">Go to market</li>
+        <li className="list-group-item">Bills</li>
       </ul>
       <div className="card-body">
         <a href="#" className="card-link">View all</a>
