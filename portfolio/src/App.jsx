52e78dac4bba8ffa1c2a1e23b6dcd80dbf06a004
vanshika704 <sharmavanshi704@gmail.com>
2024-05-25 11:40:57 +0530

    hehehehehhehe

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { Social } from "./social.jsx"; // Correctly import the Social component
 
 function App() {
   return (
@@ -100,23 +99,7 @@ function Intro() {
         <h5>I am Vanshika Sharma, a Btech Computer Science student.</h5><br />
         <h5>I work on Flutter, Firebase, React and Canva.</h5>
         <hr />
-        <div className="socialmedia">
-          <div className="icon">
-            <a href="https://www.instagram.com/sharmavanshika__________?igsh=bmlyaHU5ZjB2NWt3" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-          </div>
-          <div className="icon">
-            <a href="https://www.linkedin.com/in/vanshika-sharma-b1387428a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app " target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-          </div>
-          <div className="icon">
-            <a href="https://x.com/Vanshika310819?t=r3DQbhhaEJqFpATOyUismQ&s=09" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-          </div>
-        </div>
+        <Social />
       </div>
     </div>
   );
