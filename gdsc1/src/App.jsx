7de3f5d0974038a5a3a3b934f4229a47691c2821
vanshika704 <sharmavanshi704@gmail.com>
2024-05-27 17:26:56 +0530

    background

--- gdsc1/src/App.jsx
+++ gdsc1/src/App.jsx
@@ -1,10 +1,32 @@
-function App() {
- 
+import "./index.css";
 
+function App() {
   return (
-   <>
-   <h1>HELLO WORLD</h1></>
-  )
+    <Logincard />
+  );
 }
 
-export default App
+export default App;
+
+function Logincard() {
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+     
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the bulk of the  content.
+        </p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="card-link">Card link</a>
+        <a href="#" className="card-link">Another link</a>
+      </div>
+    </div>
+  );
+}
