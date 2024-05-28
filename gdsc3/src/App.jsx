602fad820a867220517313d755eadcb86aad256a
vanshika704 <sharmavanshi704@gmail.com>
2024-05-28 19:55:10 +0530

    .

--- gdsc3/src/App.jsx
+++ gdsc3/src/App.jsx
@@ -1,5 +1,8 @@
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-regular-svg-icons';
 
 function App() {
   return (
@@ -8,20 +11,33 @@ function App() {
         <h1>TODO LIST</h1>
         <div className="Row1">
           <div className="searchbar-container">
-            <input className="searchbar" type="text" placeholder="Search Notes"></input>
-            
-          
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-ALL
-            </button>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-            </ul></div>
+            <input className="searchbar" type="text" placeholder="Search Notes" />
+            <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                ALL
+              </button>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Important</a></li>
+                <li><a className="dropdown-item" href="#">Professional</a></li>
+              </ul>
+            </div>
+            <button className="btn btn-secondary icon"><FontAwesomeIcon icon={faMoon} /></button>
           </div>
-        </div>
+        </div><div className="List">
+        <ul className="list-group">
+          <li className="list-group-item">
+            <input className="form-check-input me-1 firsttodo" type="checkbox" value="" id="firstCheckbox" />
+            <label className="form-check-label" htmlFor="firstCheckbox">First checkbox</label>
+          </li>
+          <li className="list-group-item">
+            <input className="form-check-input me-1" type="checkbox" value="" id="secondCheckbox" />
+            <label className="form-check-label" htmlFor="secondCheckbox">Second checkbox</label>
+          </li>
+          <li className="list-group-item">
+            <input className="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox" />
+            <label className="form-check-label" htmlFor="thirdCheckbox">Third checkbox</label>
+          </li>
+        </ul></div>
       </div>
     </>
   );
