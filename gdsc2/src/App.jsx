aa2a3b1a7614ee9d1b16848b4bc5d15f842dfa95
vanshika704 <sharmavanshi704@gmail.com>
2024-05-28 12:21:06 +0530

    css

--- gdsc2/src/App.jsx
+++ gdsc2/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1>TODO LIST</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+          <div>
+            <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+              <svg
+                className="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                {/* ... */}
+              </svg>
+            </span>
+          </div>
+          <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+            Writes Upside-Down
+          </h3>
+          <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+            The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
