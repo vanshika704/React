96ee10b5c346f0919a9a54772c58365a62231909
vanshika704 <sharmavanshi704@gmail.com>
2024-05-28 19:57:29 +0530

    thu thu njr na lgje

--- gdsc3/src/App.jsx
+++ gdsc3/src/App.jsx
@@ -23,21 +23,23 @@ function App() {
             </div>
             <button className="btn btn-secondary icon"><FontAwesomeIcon icon={faMoon} /></button>
           </div>
-        </div><div className="List">
-        <ul className="list-group">
-          <li className="list-group-item">
-            <input className="form-check-input me-1 firsttodo" type="checkbox" value="" id="firstCheckbox" />
-            <label className="form-check-label" htmlFor="firstCheckbox">First checkbox</label>
-          </li>
-          <li className="list-group-item">
-            <input className="form-check-input me-1" type="checkbox" value="" id="secondCheckbox" />
-            <label className="form-check-label" htmlFor="secondCheckbox">Second checkbox</label>
-          </li>
-          <li className="list-group-item">
-            <input className="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox" />
-            <label className="form-check-label" htmlFor="thirdCheckbox">Third checkbox</label>
-          </li>
-        </ul></div>
+        </div>
+        <div className="List">
+          <ul className="list-group">
+            <li className="list-group-item">
+              <input className="form-check-input me-1 firsttodo" type="checkbox" value="" id="firstCheckbox" />
+              <label className="form-check-label" htmlFor="firstCheckbox">First checkbox</label>
+            </li>
+            <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" id="secondCheckbox" />
+              <label className="form-check-label" htmlFor="secondCheckbox">Second checkbox</label>
+            </li>
+            <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox" />
+              <label className="form-check-label" htmlFor="thirdCheckbox">Third checkbox</label>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
