b7c38a945d7c2769a94226b202e90a660e11fa00
vanshika704 <sharmavanshi704@gmail.com>
2024-05-28 12:25:59 +0530

    config

--- gdsc2/tailwind.config.js
+++ gdsc2/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/App.jsx"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class', // Enable dark mode with the class strategy
 }
-
