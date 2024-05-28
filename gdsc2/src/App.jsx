aa6f6197f4c4234deb71dd2f18f31a44397fd1f5
vanshika704 <sharmavanshi704@gmail.com>
2024-05-28 15:29:51 +0530

    push

--- gdsc2/src/App.jsx
+++ gdsc2/src/App.jsx
@@ -1,5 +1,27 @@
-import "./index.css"
-function App(){
-  return (<><h1>TODO LIST</h1></>)
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl font-bold">TODO LIST</h1>
+      <div>
+        <nav className="navbar navbar-transparent"> 
+          <div className="container-fluid">
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2" 
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-info" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
+
 export default App;
