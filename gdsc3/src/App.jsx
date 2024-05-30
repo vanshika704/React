9f16b48e35383be65a4c88108796609bc866e541
vanshika704 <sharmavanshi704@gmail.com>
2024-05-30 18:36:55 +0530

    ..

--- gdsc3/src/App.jsx
+++ gdsc3/src/App.jsx
@@ -48,15 +48,15 @@ function TodoList() {
       <div className="List">
         <ul className="list-group">
           <li className="list-group-item">
-            <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox" />
+            <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"/>
             <label className="form-check-label" htmlFor="firstCheckbox">First checkbox</label>
           </li>
           <li className="list-group-item">
-            <input className="form-check-input me-1" type="checkbox" value="" id="secondCheckbox" />
+            <input className="form-check-input me-1" type="checkbox" value="" id="secondCheckbox"/>
             <label className="form-check-label" htmlFor="secondCheckbox">Second checkbox</label>
           </li>
           <li className="list-group-item">
-            <input className="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox" />
+            <input className="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox"/>
             <label className="form-check-label" htmlFor="thirdCheckbox">Third checkbox</label>
           </li>
         </ul>
