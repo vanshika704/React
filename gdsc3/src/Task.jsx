9fa61def831f4530a285c18678ad8adcfc92a9a9
vanshika704 <sharmavanshi704@gmail.com>
2024-05-30 13:02:37 +0530

    ...

--- gdsc3/src/Task.jsx
+++ gdsc3/src/Task.jsx
@@ -1,9 +1,35 @@
-import './index.css';
+import { useState, useEffect } from 'react';
 
 function Task() {
+  const [taskInput, setTaskInput] = useState('');
+
+  const handleChange = (e) => {
+    setTaskInput(e.target.value);
+  };
+
+  useEffect(() => {
+    // Retrieve value from localStorage if exists
+    const storedValue = localStorage.getItem('taskInput');
+    if (storedValue) {
+      setTaskInput(storedValue);
+    }
+  }, []);
+
+  const handleSave = () => {
+    // Save input value to localStorage
+    localStorage.setItem('taskInput', taskInput);
+  };
+
   return (
     <div className="center-container">
-      <input className="addtodo" type="text" placeholder="Enter Task" />
+      <input 
+        className="addtodo" 
+        type="text" 
+        placeholder="Enter Task" 
+        value={taskInput} 
+        onChange={handleChange} 
+        onBlur={handleSave} 
+      />
     </div>
   );
 }
