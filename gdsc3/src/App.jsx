83b21892a6ac6d5d84737e11645ecd7af73d17b3
vanshika704 <sharmavanshi704@gmail.com>
2024-05-31 17:52:35 +0530

    .

--- gdsc3/src/App.jsx
+++ gdsc3/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';// importing for navigation
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
