dc9903ce211041529d2eec35056e96038a1afed2
vanshika704 <sharmavanshi704@gmail.com>
2024-06-06 17:43:43 +0530

    ...

--- FeastFriends/src/App.jsx
+++ FeastFriends/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './index.css'; // Ensure this points to your CSS file
-import "bootstrap/dist/css/bootstrap.min.css"
+
 function App() {
   return (
     <>
@@ -21,8 +21,8 @@ function Credentials() {
     <div className="credentials">
       <input type="text" placeholder="Enter Email" />
       <input type="password" placeholder="Enter Password" />
-      <button>Login</button>
-      <button>Signin with Google</button>
+      <button className="custom-button">Sign In with Google </button>
+
     </div>
   );
 }
