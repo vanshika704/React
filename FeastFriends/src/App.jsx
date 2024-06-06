78fa3c2016565d502441b0e8fb82e23fb8bdf041
vanshika704 <sharmavanshi704@gmail.com>
2024-06-06 20:55:25 +0530

    .

--- FeastFriends/src/App.jsx
+++ FeastFriends/src/App.jsx
@@ -1,13 +1,28 @@
-
 import './index.css'; // Ensure this points to your CSS file
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/Page2" element={<Page2 />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
   return (
     <>
       <Logo />
       <Credentials />
     </>
   );
+}
+
+function Page2() {
+  return <div>This is Page 2 content!</div>;
 }
 
 function Logo() {
@@ -21,8 +36,9 @@ function Credentials() {
     <div className="credentials">
       <input type="text" placeholder="Enter Email" />
       <input type="password" placeholder="Enter Password" />
-      <button className="custom-button">Sign In with Google </button>
-
+      <Link to="/Page2">
+        <button className="custom-button">Sign In with Google</button>
+      </Link>
     </div>
   );
 }
---
+++ FeastFriends/src/Page2.jsx
@@ -0,0 +1,11 @@
+
+
+function Page2() {
+  return (
+    <div>
+      <h1>Welcome to Page 2</h1>
+    </div>
+  );
+}
+
+export default Page2;
