27de7be1e7cfd73251ee40568b61a97bf7759070
vanshika704 <sharmavanshi704@gmail.com>
2024-06-11 23:18:32 +0530

    done

--- FeastFriends/src/appbar.jsx
+++ FeastFriends/src/appbar.jsx
@@ -5,31 +5,29 @@ function AppBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-black">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <div className="explore">
-            <img src="src/assets/logo.png" alt="Logo" className="navlogo" />
-            <hr className="breadcrumb-divider" />
-            <Breadcrumb spacing="10px" separator=" ">
-              <BreadcrumbItem>
-                <BreadcrumbLink href="#">Home</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbItem>
-                <BreadcrumbLink href="#">Explore</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbItem>
-                <BreadcrumbLink href="#">Partner</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbItem>
-                <BreadcrumbLink href="#">About</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbItem>
-                <BreadcrumbLink href="#">Contact</BreadcrumbLink>
-              </BreadcrumbItem>
-            </Breadcrumb>
-          </div>
-        </a>
+        <div className="imagecontainer">
+          <img src="src/assets/logo.png" alt="Logo" className="navlogo" />
+        </div>
+        <Breadcrumb spacing="10px" separator=" " className="breadcrumb-container">
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">Explore</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">Partner</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">About</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">Contact</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
       </div>
     </nav>
   );
 }
+
 export default AppBar;
