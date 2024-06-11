745433b0cd132c66bc2aa62564689245d99682f0
vanshika704 <sharmavanshi704@gmail.com>
2024-06-11 21:20:37 +0530

    .

--- FeastFriends/src/main.jsx
+++ FeastFriends/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './Home';
+import App from './App.jsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Home />
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
