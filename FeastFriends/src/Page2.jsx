dfe7272663735f4047d8d33de657494a562b5363
vanshika704 <sharmavanshi704@gmail.com>
2024-06-12 22:39:49 +0530

    .

--- FeastFriends/src/Page2.jsx
+++ FeastFriends/src/Page2.jsx
@@ -8,17 +8,22 @@ function MainPage() {
       <div className='MainPagebody'>
         <Food1 />
         
-      </div><Info/>
+      </div><Info />
     </div>
   );
 }
 
 export default MainPage;
 
+
 function Food1() {
   return (
     <div className="image-container">
       <img src="src/assets/food3.jpg" className='imagemain' alt="Food" />
+      <div className="text-overlay">
+       <h1>Life is Too Short For<br/> Boring food</h1> 
+       <h6 >Eat Something Great Everyday In Your Budget</h6>
+      </div> 
       <div className="grey-container">
         <p>This is a grey container</p>
       </div>
@@ -26,6 +31,6 @@ function Food1() {
   );
 }
 
-function Info(){
-  return(<div className='info'>hello</div>)
+function Info() {
+  return (<div className='info'>hello</div>)
 }
