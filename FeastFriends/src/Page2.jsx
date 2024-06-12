7604a7af4d26fa6dbc4d6a1b889e1132db4b7a06
vanshika704 <sharmavanshi704@gmail.com>
2024-06-12 21:46:53 +0530

    .

--- FeastFriends/src/Page2.jsx
+++ FeastFriends/src/Page2.jsx
@@ -1,17 +1,27 @@
 import AppBar from './appbar';
 import "./index.css";
+
 function MainPage() {
   return (
     <div>
       <AppBar />
       <div className='MainPagebody'>
-       <Food1/>
+        <Food1 />
       </div>
     </div>
   );
 }
 
 export default MainPage;
-function Food1(){
-  return(<img src="src/assets/food3.jpg" className='imagemain'/>)
+
+function Food1() {
+  return (
+    <div className="image-container">
+      <img src="src/assets/food3.jpg" className='imagemain' alt="Food" />
+      <div className="grey-container">
+        <p>This is a grey container</p>
+      </div>
+    </div>
+  );
 }
+
