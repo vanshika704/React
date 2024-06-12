bdbc0d6ae9b4312a35fff74285044f1a1bf2f535
vanshika704 <sharmavanshi704@gmail.com>
2024-06-12 21:10:14 +0530

    .

--- FeastFriends/src/Page2.jsx
+++ FeastFriends/src/Page2.jsx
@@ -13,5 +13,5 @@ function MainPage() {
 
 export default MainPage;
 function Food1(){
-  return(<img src="src/assets/food9.jpg" alt="Logo" width={1200} height={400} className='imagemain'/>)
+  return(<img src="src/assets/food3.jpg" className='imagemain'/>)
 }
