63e8a1d5d12967e39fdc01640add57a50b84d68f
vanshika704 <sharmavanshi704@gmail.com>
2024-06-15 00:37:52 +0530

    appbar done

--- socialmedia/src/components/Appbar.jsx
+++ socialmedia/src/components/Appbar.jsx
@@ -1,4 +1,3 @@
-
 import { NavLink } from 'react-router-dom';
 
 const TabBar = () => {
@@ -6,14 +5,18 @@ const TabBar = () => {
         <div className="tab-bar d-flex justify-content-between align-items-center bg-light p-3">
             <div className="tab-links">
                 <NavLink exact to="/" className="tab-link" activeClassName="active">Home</NavLink>
-                <NavLink to="/features" className="tab-link ml-3" activeClassName="active">Features</NavLink>
-                <NavLink to="/pricing" className="tab-link ml-3" activeClassName="active">Pricing</NavLink>
-                <NavLink to="/faqs" className="tab-link ml-3" activeClassName="active">FAQs</NavLink>
-                <NavLink to="/about" className="tab-link ml-3" activeClassName="active">About</NavLink>
+                &nbsp;&nbsp;&nbsp;
+                <NavLink to="/features" className="tab-link" activeClassName="active">Features</NavLink>
+                &nbsp;&nbsp;&nbsp;
+                <NavLink to="/pricing" className="tab-link" activeClassName="active">Pricing</NavLink>
+                &nbsp;&nbsp;&nbsp;
+                <NavLink to="/faqs" className="tab-link" activeClassName="active">FAQs</NavLink>
+                &nbsp;&nbsp;&nbsp;
+                <NavLink to="/about" className="tab-link" activeClassName="active">About</NavLink>
             </div>
             <div className="search-bar d-flex align-items-center ml-auto">
-                <input type="text" className="form-control mr-2" placeholder="Search..." />
                 <button type="button" className="btn btn-primary mr-2">Search</button>
+                <input type="text" className="form-control mr-2" placeholder="Search..." />
                 <button type="button" className="btn btn-outline-primary">Login</button>
             </div>
         </div>
