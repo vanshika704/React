fbba6b2c04ea3cee7e2babf27a119e2bbd46f72d
vanshika704 <sharmavanshi704@gmail.com>
2024-06-15 18:24:51 +0530

    ..

--- socialmedia/src/components/Appbar.jsx
+++ socialmedia/src/components/Appbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 import '../index.css';
 import Sidebar from './sidebar';
-
+import Signup from './signup';
 const TabBar = () => {
     return (
         <div className="tab-bar d-flex justify-content-between align-items-center bg-dark p-3">
@@ -23,7 +23,7 @@ const TabBar = () => {
                
                 <input type="text" className="form-control mr-2" />
                 <button type="button" className="btn btn-outline-warning">Login</button>
-                <button type="button" className="btn btn-warning mr-2">Signup</button>
+                <Signup/>
             </div>
         </div>
     );
---
+++ socialmedia/src/components/signup.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import {
+  Button,
+  Box,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Select,
+  Textarea,
+  Stack,
+} from '@chakra-ui/react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
+import { useDisclosure } from '@chakra-ui/react'; // Import useDisclosure from Chakra UI
+import { FaPlus } from 'react-icons/fa';
+
+function Signup() {
+  const { isOpen, onOpen, onClose } = useDisclosure(); // Initialize useDisclosure
+
+  const firstField = React.useRef();
+
+  return (
+    <>
+      <Button
+        leftIcon={<FaPlus />}
+        fontSize="sm"
+        colorScheme='warning' // Set colorScheme to 'warning' for warning color
+        variant="solid" // Use 'solid' variant for filled button
+        onClick={onOpen}
+        color="white" // Text color set to white
+      >
+        Signup
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        initialFocusRef={firstField}
+        onClose={onClose}
+      >
+        <DrawerOverlay />
+        <DrawerContent bg='gray.800' color='white'> {/* Set background color to gray.800 and text color to white */}
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth='1px'>
+            Create a new account
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Stack spacing='24px'>
+              <Box>
+                <FormLabel htmlFor='username'>Name</FormLabel>
+                <Input
+                  ref={firstField}
+                  id='username'
+                  placeholder='Please enter user name'
+                />
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor='url'>Url</FormLabel>
+                <InputGroup>
+                  <InputLeftAddon>http://</InputLeftAddon>
+                  <Input
+                    type='url'
+                    id='url'
+                    placeholder='Please enter domain'
+                  />
+                  <InputRightAddon>.com</InputRightAddon>
+                </InputGroup>
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor='owner'>Select Owner</FormLabel>
+                <Select id='owner' defaultValue='segun'>
+                  <option value='segun'>Segun Adebayo</option>
+                  <option value='kola'>Kola Tioluwani</option>
+                </Select>
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor='desc'>Description</FormLabel>
+                <Textarea id='desc' />
+              </Box>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter borderTopWidth='1px'>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Submit</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
+
+export default Signup;
