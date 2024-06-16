c5ec20cccbc58f52c96b526f36c87dadca9fe5f3
vanshika704 <sharmavanshi704@gmail.com>
2024-06-16 17:32:58 +0530

    ..

--- socialmedia/src/components/footer.jsx
+++ socialmedia/src/components/footer.jsx
@@ -1,13 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../index.css'; 
-
+import { Image } from '@chakra-ui/react'
 const Footer = () => {
     return (
         <div className="footer d-flex justify-content-between align-items-center bg-dark p-3">
             <div className="company-info d-flex align-items-center">
-                <img src="/path/to/logo.png" alt="Company Logo" className="logo mr-2" />
-                <span className="company-name text-white">Company Name</span>
+            <Image
+  borderRadius='full'
+  boxSize='50px'
+  src='https://bit.ly/dan-abramov'
+  alt='Dan Abramov'
+/>
+                <span className="company-name text-white">Hey Sam!!!</span>
             </div>
             <div className="social-icons">
                 <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white mr-5">
