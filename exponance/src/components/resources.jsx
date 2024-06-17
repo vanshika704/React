6ef904b3db04262d80776ee51be12cdf5b723b25
vanshika704 <sharmavanshi704@gmail.com>
2024-06-18 00:04:06 +0530

    .

--- exponance/src/components/resources.jsx
+++ exponance/src/components/resources.jsx
@@ -2,6 +2,7 @@ import "../index.css";
 function Resources (){
     return(<div className="Resources">
         <div className="Resourcesand">Resources and Insights</div>
+        <div className="text2">The latest industry news,interviews,technologies and resources</div>
     </div>)
 }
 export default Resources;
