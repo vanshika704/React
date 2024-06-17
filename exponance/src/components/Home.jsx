913d8f14764ca074fd8b955f418adf406d2b93b7
vanshika704 <sharmavanshi704@gmail.com>
2024-06-17 23:09:18 +0530

    .

--- exponance/src/components/Home.jsx
+++ exponance/src/components/Home.jsx
@@ -2,6 +2,6 @@ import Appbar from "./Appbar";
 import "../index.css";
 function Home (){
     return (<div className="Home"><Appbar/>
-    <div className="mainpagetext">If you want <br/> to grow,<br/> just grow <br/>Exponentially</div></div>)
+    <div className="mainpagetext">If you want <br/> to grow,<br/> just grow <br/>Exponentially<br/></div><button className="Startnow" >Start Now ----&gt;</button></div>)
 }
 export default Home;
