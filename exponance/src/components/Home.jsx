9befdda680500528ed9a932c4575792b5c79324f
vanshika704 <sharmavanshi704@gmail.com>
2024-06-18 00:00:23 +0530

    .

--- exponance/src/components/Home.jsx
+++ exponance/src/components/Home.jsx
@@ -1,7 +1,35 @@
-import Appbar from "./Appbar";
-import "../index.css";
-function Home (){
-    return (<div className="Home"><Appbar/>
-    <div className="mainpagetext">If you want <br/> to grow,<br/> just grow <br/>Exponentially<br/></div><button className="Startnow" >Start Now ----&gt;</button></div>)
+
+import '../index.css'; 
+
+import Appbar from './Appbar';
+import Resources from './resources';
+
+function Home() {
+  return (<>
+    <div className="Home">
+      <Appbar />
+      <div className='imageandtext'>
+        <div className="mainpagetext">
+          <div>
+            If you want <br />
+            to grow,<br />
+            just grow <br />
+            Exponentially<br />
+          </div>
+          <button className="Startnow">Start Now --&gt;</button>
+        </div>
+        
+        <div className="image-stack">
+          <img className='image2' src="src/assets/Card 1.png" alt="First Image" height={500} width={500} />
+          <img className='image1' src="src/assets/Card 2.png" alt="Second Image" height={500} width={500} />
+        </div>
+      </div>
+     
+    </div>
+    <Resources/>
+    </>
+  );
 }
+
 export default Home;
+
---
+++ exponance/src/components/resources.jsx
@@ -0,0 +1,7 @@
+import "../index.css";
+function Resources (){
+    return(<div className="Resources">
+        <div className="Resourcesand">Resources and Insights</div>
+    </div>)
+}
+export default Resources;
