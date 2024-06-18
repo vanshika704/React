7f73665e1cffdc9dd83513cbc12ee43b57af7eb7
vanshika704 <sharmavanshi704@gmail.com>
2024-06-18 11:47:48 +0530

    .

--- exponance/src/components/page3.jsx
+++ exponance/src/components/page3.jsx
@@ -1,4 +1,14 @@
-function Page3(){
-    return(<div className="Page3"></div>)
+
+import "../index.css";
+
+function Page3() {
+  return (
+    <div className="Page3">
+      <div className="page3imageandtext">
+        <img className="image3" src= 'src/assets/Other 12.png' height={400} width={400} alt="Description of image" />
+      </div>
+    </div>
+  );
 }
+
 export default Page3;
