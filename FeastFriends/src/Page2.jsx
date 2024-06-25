cb05fe295db8fbd766cdfd554e1cc81436846353
vanshika704 <sharmavanshi704@gmail.com>
2024-06-25 13:24:21 +0530

    .

--- FeastFriends/src/Page2.jsx
+++ FeastFriends/src/Page2.jsx
@@ -66,10 +66,10 @@ function Info() {
   templateColumns='repeat(5, 1fr)'
   gap={4}
 >
-  <GridItem rowSpan={2} colSpan={1} bg='tomato' />
-  <GridItem colSpan={2} bg='papayawhip' />
-  <GridItem colSpan={2} bg='papayawhip' />
-  <GridItem colSpan={4} bg='tomato' />
+  <GridItem rowSpan={2} colSpan={1} bg='tomato'></GridItem>
+  <GridItem colSpan={2} bg='papayawhip' ></GridItem>
+  <GridItem colSpan={2} bg='papayawhip'></GridItem>
+  <GridItem colSpan={4} bg='tomato' ></GridItem>
 </Grid>
     </div>
   );
