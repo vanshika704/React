138cd65ac2fc4c5316227e7eeaaef9e6ecb825d8
vanshika704 <sharmavanshi704@gmail.com>
2024-07-15 20:30:11 +0530

    abhi tkk sb raam bharose

--- genie/lib/Pages/Page1.dart
+++ genie/lib/Pages/Page1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,6 +11,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("hello"));
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 16, 33, 70),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "G.E.N.I.E",
+              style: GoogleFonts.orbitron(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 70,
+                ),
+              ),
+            ),
+            Image.asset(
+              "assets/genielogo.png",
+              height: 300,
+              width: 300,
+            ),
+            Text(
+              "Green Environmental Network \n     For Intelligent Ecosystem",
+              style: GoogleFonts.orbitron(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
