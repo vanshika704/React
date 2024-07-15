126d857f59fc77d1903ed078cef36366f2a65358
vanshika704 <sharmavanshi704@gmail.com>
2024-07-15 19:32:49 +0530

    lib

--- genie/lib/main.dart
+++ genie/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Genie',
       home: MyHomePage(),
     );
   }
@@ -26,6 +26,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: Text("hello"));
   }
 }
