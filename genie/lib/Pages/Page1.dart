5e8b5855463e63aefa251c85ca7727ac198b48a8
vanshika704 <sharmavanshi704@gmail.com>
2024-07-15 20:45:03 +0530

    ayila jaadu

--- genie/lib/Pages/Page1.dart
+++ genie/lib/Pages/Page1.dart
@@ -12,35 +12,42 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 16, 33, 70),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "G.E.N.I.E",
-              style: GoogleFonts.orbitron(
-                textStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 70,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/why-sky-blue-2db86ae.webp"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "G.E.N.I.E",
+                style: GoogleFonts.orbitron(
+                  textStyle: const TextStyle(
+                    color: Color.fromARGB(255, 16, 33, 70),
+                    fontSize: 70,
+                  ),
                 ),
               ),
-            ),
-            Image.asset(
-              "assets/genielogo.png",
-              height: 300,
-              width: 300,
-            ),
-            Text(
-              "Green Environmental Network \n     For Intelligent Ecosystem",
-              style: GoogleFonts.orbitron(
-                textStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+              Image.asset(
+                "assets/genielogo.png",
+                height: 300,
+                width: 300,
+              ),
+              Text(
+                "Green Environmental Network \n     For Intelligent Ecosystem",
+                style: GoogleFonts.orbitron(
+                  textStyle: const TextStyle(
+                    color: Color.fromARGB(255, 16, 33, 70),
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
